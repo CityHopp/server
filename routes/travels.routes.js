9fa2fbc12aebb7a0a83b52068fc7d62a02e7025a
Antoine Quint <antoine.quint4@gmail.com>
2025-03-05 12:38:45 +0100

    Made get and post work

--- routes/travels.routes.js
+++ routes/travels.routes.js
@@ -3,31 +3,31 @@ const router = express.Router();
 
 const Travel = require("../models/Travel.model");
 
-router.get("/travels", (req, res) => {
-  res.json("All good in here");
+router.get("/", (req, res) => {
   Travel.find()
   .then((travelsFromDb) => {
-        res.status(200).json(travelsFromDb);
+        res.status(200).json({data: travelsFromDb});
       })
       .catch((e) => next(e));
 });
-// router.post("/travels",(req,res,next) =>{
-//     Travel.create({
-//         destination: req.body.destination,
-//         startingCity:req.body.startingCity,
-//         departingTime:req.body.departingTime,
-//         date: req.body.date,
-//         breaks: req.body.breaks,
-//         petPolicy: req.body.petPolicy,
-//         kidPolicy: req.body.kidPolicy,
-//         smokingPolicy:req.body.smokingPolicy,
-//         chitChatPolicy: req.body.chitChatPolicy,
-//         stops:req.body.stops,
-//         price: req.body.price,
-//         description:req.body.description
-//         .then((newTravel) => {
-//             res.status(200).json({data:newTravel});
-//           })
-// })
-// });
+router.post("/",(req,res,next) =>{
+    Travel.create({
+        destination: req.body.destination,
+        startingCity:req.body.startingCity,
+        departingTime:req.body.departingTime,
+        date: req.body.date,
+        breaks: req.body.breaks,
+        petPolicy: req.body.petPolicy,
+        kidPolicy: req.body.kidPolicy,
+        smokingPolicy:req.body.smokingPolicy,
+        chitChatPolicy: req.body.chitChatPolicy,
+        stops:req.body.stops,
+        price: req.body.price,
+        description:req.body.description
+    })
+        .then((newTravel) => {
+            res.status(200).json({data:newTravel});
+          })
+
+});
 module.exports = router;
